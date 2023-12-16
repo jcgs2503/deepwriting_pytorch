966b693291ef4ecc58e55029f12bdc781a4f18ac
jcgs2503 <jcgs2503@gmail.com>
2023-12-15 22:52:31 -0600

    create project structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Pytorch version of the Deepwriting project',
+    author='Jerry Chang, Ian Huang',
+    license='MIT',
+)
